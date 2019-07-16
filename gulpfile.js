535eef716f376d3eefc7f9c6edb3f83a1804af60
unknown <malyuk1313@gmail.com>
2019-07-16 23:47:05 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+    return gulp.src('app/sass/*.scss')
+        .pipe(sass.sync())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+})
+
+gulp.task('images', function () {
+    return gulp.src('app/img/*.*')
+        .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'))
+})
+gulp.task('build', gulp.series(
+    'sass',
+    'images',
+    'html'
+))
+
+gulp.task('serve', gulp.series('build', () => {
+    browserSync.init({ server: "dist" });
+    gulp.watch("app/sass/*.scss", gulp.series('sass'));
+    gulp.watch("app/img/*.*", gulp.series('images'));
+    gulp.watch("app/*.html", gulp.series('html')).on('change', browserSync.reload);
+}));
