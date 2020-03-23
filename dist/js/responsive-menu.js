71e73ace74a9bf46dec513e8eaa0921b5a5a5610
Malyukin Andrew <malyuk1313@gmail.com>
2020-03-23 21:03:46 +0200

    some changes in mobile menu

--- dist/js/responsive-menu.js
+++ dist/js/responsive-menu.js
@@ -1,5 +1,8 @@
 function responsiveMenuToggle() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("responsive");
-    document.body.style.overflow = menu.classList.contains("responsive")? "hidden":"";
+    document.body.style.overflow = menu.classList.contains("responsive")? "hidden":document.body.removeAttribute("style");
   }
+function responsiveMenuToggleCheck(){
+  menu.classList.contains("responsive")?responsiveMenuToggle():"";
+}
