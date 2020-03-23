c9f1edb9ea59af4cc479ffb6d7c89636d39c75ac
Malyukin Andrew <malyuk1313@gmail.com>
2020-03-23 22:56:15 +0200

    added smooth scroll animation

--- dist/js/responsive-menu.js
+++ dist/js/responsive-menu.js
@@ -1,8 +1,16 @@
 function responsiveMenuToggle() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("responsive");
-    document.body.style.overflow = menu.classList.contains("responsive")? "hidden":document.body.removeAttribute("style");
-  }
-function responsiveMenuToggleCheck(){
-  menu.classList.contains("responsive")?responsiveMenuToggle():"";
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("responsive");
+  document.body.style.overflow = menu.classList.contains("responsive") ? "hidden" : document.body.removeAttribute("style");
 }
+function responsiveMenuToggleCheck() {
+  menu.classList.contains("responsive") ? responsiveMenuToggle() : "";
+}
+
+$('.nav-link').click(function(event) {
+  const selector = $(this).attr("href"); 
+  $('html, body').animate({
+    scrollTop: $(selector).offset().top
+  }, 1000);
+  
+})
